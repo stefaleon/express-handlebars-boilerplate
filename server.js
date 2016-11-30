86eaef28480586355b4d665609c87df3becba10c
maimous@gmail.com <stefaleon@gmail.com>
2016-11-30 14:17:42 +0200

    add log to file functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +12,13 @@ app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
 	var now = new Date().toUTCString();
-	console.log(`${now}: ${req.method} ${req.url}`);
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (error) => {
+		if (error) {
+			console.log('Unable to append to server.log.');
+		}
+	});
 	next();
 });
 
